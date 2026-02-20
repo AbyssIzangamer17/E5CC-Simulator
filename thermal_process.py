1340b17777266809ae88f15bd27f3737d4d386c1
AbyssIzangamer17 <abyssizangamer17@gmail.com>
2026-02-20 14:50:43 +0100

    Refine thermal physics: Reduced heater gain to soften power pulses for better stability

--- thermal_process.py
+++ thermal_process.py
@@ -1,13 +1,13 @@
 import time
 
 class ThermalProcess:
-    def __init__(self, initial_temp=25.0, ambient_temp=25.0, gain=0.8, tau=150.0, lag=3.0):
+    def __init__(self, initial_temp=25.0, ambient_temp=25.0, gain=0.4, tau=150.0, lag=4.0):
         self.true_temperature = initial_temp
         self.displayed_temperature = initial_temp
         self.ambient_temp = ambient_temp
-        self.k = gain  # Efficiency (Lowered for stability)
-        self.tau = tau  # Thermal mass (Increased for stability)
-        self.lag_tau = lag # Sensor lag
+        self.k = gain  # Lowered gain to reduce "pulse" impact
+        self.tau = tau  # Thermal mass
+        self.lag_tau = lag # Increased sensor lag
         self.last_time = time.time()
 
     def update(self, power, disturbance=0.0):
